c519df33b17247a511d9f009666f63feb8f29dab
maria.hernandez <maria.hernandez@loopup.com>
2017-05-07 15:34:45 -0700

    blue girl fixes

---
+++ Assets/Scripts/MonoBehaviours/FallingBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBehaviour : MonoBehaviour {
+
+	private bool isFalling;
+	private Animator theAnimator;
+
+	// Use this for initialization
+	void Start () {
+		isFalling = false;
+		theAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionExit2D(Collision2D collision) {
+		if (collision.gameObject.tag == "HousePlatform") {
+			theAnimator.SetBool("Falling", true);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == "Ground") {
+			theAnimator.SetBool("Falling", false);
+			theAnimator.SetBool("Walking", true);
+		}
+	}
+}
