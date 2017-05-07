cdc3841fc8c9f9a19495641b5222b502220b62c5
maria.hernandez <maria.hernandez@loopup.com>
2017-05-07 07:51:42 -0700

    speech bubble prefab (canvas with text + script)

---
+++ Assets/Scripts/MonoBehaviours/SpeechBubble.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechBubble : MonoBehaviour {
+
+	public string textToShow;
+	public Canvas theCanvas;
+
+	// Use this for initialization
+	void Start () {
+		theCanvas.enabled = false;
+		Transform textChild = theCanvas.transform.Find("Text");
+		textChild.GetComponent<Text>().text = textToShow;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Player") {
+			theCanvas.enabled = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "Player") {
+			theCanvas.enabled = true;
+		}
+	}
+
+
+}
--- Assets/Scripts/MonoBehaviours/UseFromInventory.cs
+++ Assets/Scripts/MonoBehaviours/UseFromInventory.cs
@@ -7,8 +7,6 @@ public class UseFromInventory : MonoBehaviour {
 	// Key for activating the option to use an object from the inventory
 	// This can be removed and be automatic when the user enters the trigger
 	public KeyCode activateKey;
-	public string title = "You can use an item from the inventory now!";
-
 
 	private bool isActive;
 	private Inventory theInventory;
@@ -20,27 +18,28 @@ public class UseFromInventory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyUp(activateKey)) {
+		// TODO: commented from now (the user walking close by will
+		// activate the inventory directly, no need to extra clicks)
+		/*if(Input.GetKeyUp(activateKey)) {
 			if(isActive) {
 				// TODO: Show text to use an item from inventory
 				print(">>>>>>>"  + title);
 				theInventory.EnableUsingAnItem();
 			}
-		}
+		}*/
 	}
 
 	// Whenever player hits the trigger (it is enough close)
 	// the item will be active
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "Player") {
-			print(">>>>>>> Press key to activate this item");
 			isActive = true;
+			theInventory.EnableUsingAnItem();
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
 		if (other.tag == "Player") {
-			// TODO: Hide text to use an item from inventory
 			isActive = false;
 			theInventory.DisableUsingAnItem();
 		}
