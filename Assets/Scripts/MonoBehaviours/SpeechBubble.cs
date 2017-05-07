9d895fb18867c1d0e259e6c4272750e1262a17eb
maria.hernandez <maria.hernandez@loopup.com>
2017-05-07 08:25:01 -0700

    speech bubble image

--- Assets/Scripts/MonoBehaviours/SpeechBubble.cs
+++ Assets/Scripts/MonoBehaviours/SpeechBubble.cs
@@ -23,7 +23,7 @@ public class SpeechBubble : MonoBehaviour {
 
 	void OnTriggerExit2D(Collider2D other) {
 		if (other.tag == "Player") {
-			theCanvas.enabled = true;
+			theCanvas.enabled = false;
 		}
 	}
 
