c7c02f50b0ab7b97b667b9c3ddf8fb91748244a4
maria.hernandez <maria.hernandez@loopup.com>
2017-05-06 23:44:18 -0700

    collision detected on movetowards script (girls)

--- Assets/Scripts/MonoBehaviours/MoveTowards.cs
+++ Assets/Scripts/MonoBehaviours/MoveTowards.cs
@@ -22,6 +22,12 @@ public class MoveTowards : MonoBehaviour, IIsUnlockedByItem {
 		}
 	}
 
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.gameObject.tag == targetObject.tag) {
+			movementIsActive = false;
+		}
+	}
+
 	public void ItemUsed() {
 		movementIsActive = true;
 	}
